519de4dfb77ba3a17b831fd57537c147cbc8f727
marinofranz <mvbjarnasynir@gmail.com>
2024-02-24 16:03:43 +0000

    feat: add duration and session type to session

--- src/structures/Session.ts
+++ src/structures/Session.ts
@@ -1,6 +1,12 @@
 import { Base } from "./Base";
 import { Event } from "./Event";
 
+type SessionType =
+  | "practice"
+  | "qualifying"
+  | "sprint_shootout"
+  | "sprint"
+  | "race";
 export interface SessionData {
   number: number;
   name: string;
@@ -45,5 +51,46 @@ export class Session extends Base<SessionData> {
      * @type {Date}
      */
     this.date = data.date;
+  }
+
+  /**
+   * Get the type of the session.
+   * @returns {SessionType} (e.g. "practice")
+   * @readonly
+   */
+  get type(): SessionType {
+    const lowerName = this.name.toLowerCase();
+
+    if (lowerName.startsWith("practice")) return "practice";
+    if (lowerName.startsWith("qualifying")) return "qualifying";
+    if (lowerName.startsWith("sprint shootout")) return "sprint_shootout";
+    if (lowerName.startsWith("sprint")) return "sprint";
+    if (lowerName.startsWith("race")) return "race";
+
+    return "practice";
+  }
+
+  /**
+   * Get the estimated duration of the session in minutes.
+   * @returns {number}
+   * @readonly
+   */
+  get duration(): number {
+    // Pre-season testing duration is 9 hours per day.
+    if (this.event.isTesting) return 9 * 60;
+
+    // Practice, qualifying & sprint are 1 hour.
+    if (
+      this.type === "practice" ||
+      this.type === "qualifying" ||
+      this.type === "sprint"
+    )
+      return 1 * 60;
+
+    // Sprint shootout is 30 minutes.
+    if (this.type === "sprint_shootout") return 0.5 * 60;
+
+    // All other session types (race) are 2 hours.
+    return 2 * 60;
   }
 }
