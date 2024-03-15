7a7ebf94faef2c4f617e1a2ac0d31cfb59ea6ee8
marinofranz <mvbjarnasynir@gmail.com>
2024-03-15 12:50:07 +0000

    refactor: export structures

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+/* Exporting classes (structures) */
+export { Base } from "./structures/Base";
+export { Schedule } from "./structures/Schedule";
+export { Event } from "./structures/Event";
+export { Session } from "./structures/Session";
+
+/* Exporting routes */
 export * as schedule from "./routes/schedule";
+
+/* Exporting types */
 export type * from "./types";
